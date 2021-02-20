9c1daee3bfed1a05fa9e92e6bfe7319bdcfa94ba
Matthes <matthes@leuffen.de>
2021-02-20 13:12:35 +0100

    update

--- src/UpdateRunner.php
+++ src/UpdateRunner.php
@@ -12,6 +12,8 @@ class UpdateRunner
 
     private $runCount = 0;
 
+    private $currentRevison = null;
+    
     public function __construct (
         private RudlGitDbClient $gitDbClient,
         private int $defaultSleepTime = 5
@@ -21,11 +23,16 @@ class UpdateRunner
     {
         $this->runCount++;
         try {
+            while(($nextRev = $this->gitDbClient->getRevision()) === $this->currentRevison) {
+                sleep($this->defaultSleepTime);
+            }
+            echo "[" . date ("Y-m-D H:i:s") . "] New revision $this->currentRevison -> $nextRev. Triggering update...\n";
             $fn($this);
             if ($this->errorCount > 0) {
                 echo "[" . date ("Y-m-D H:i:s") . "] Recovered from previous error after " . ($this->errorCount * self::ON_ERROR_SLEEP_TIME) . " seconds System ok.\n";
             }
             $this->errorCount = 0;
+            $this->currentRevison = $nextRev;
         } catch (\Exception|\Error $ex) {
             $this->gitDbClient->logError("Exception: " . $ex->getMessage());
             $this->errorCount++;
