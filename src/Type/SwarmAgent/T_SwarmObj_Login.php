b75c8f9ca52f4860baffdea170faa9e4ecb09993
Matthes <matthes@leuffen.de>
2021-02-22 18:25:31 +0100

    update

---
+++ src/Type/SwarmAgent/T_SwarmObj_Login.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Rudl\LibGitDb\Type\SwarmAgent;
+
+
+class T_SwarmObj_Login
+{
+
+    public $registry;
+
+    public $user;
+
+    public $passwd;
+}
---
+++ src/Type/SwarmAgent/T_SwarmObj_Stack.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Rudl\LibGitDb\Type\SwarmAgent;
+
+
+class T_SwarmObj_Stack
+{
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var bool
+     */
+    public $online = true;
+
+    /**
+     * @var string
+     */
+    public $compose_file;
+}
