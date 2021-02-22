b6d8dc60cafccfe1df7bc26d08ed8f4cb044983a
Matthes <matthes@leuffen.de>
2021-02-22 10:35:30 +0100

    added hydrate / dehydrate

--- src/Type/Transport/T_Object.php
+++ src/Type/Transport/T_Object.php
@@ -34,4 +34,39 @@ class T_Object
         $this->content = $content;
         $this->encrypted = $encrypted;
     }
+
+
+    public function dehydrate($content) : void
+    {
+        $extension = pathinfo($this->name, PATHINFO_EXTENSION);
+        try {
+            if (in_array($extension, ["yml", "yaml"])) {
+                $this->content = phore_yaml_encode($content);
+            } else if ($extension === "json") {
+                $this->content = phore_json_encode($content);
+            } else {
+                throw new \InvalidArgumentException("Unrecoginized extension '$extension' for dehydration");
+            }
+        } catch (\Exception $e) {
+            throw new \InvalidArgumentException("dehydrate error in object '$this->name': " . $e->getMessage(), $e);
+        }
+    }
+
+    public function hydrate(string $class) : mixed
+    {
+        $extension = pathinfo($this->name, PATHINFO_EXTENSION);
+        try {
+            if (in_array($extension, ["yml", "yaml"])) {
+                $data = phore_yaml_decode($this->content);
+            } else if ($extension === "json") {
+                $data = phore_json_decode($this->content);
+            } else {
+                throw new \InvalidArgumentException("Unrecoginized extension '$extension' for deserialization");
+            }
+            return phore_hydrate($data, $class);
+        } catch (\Exception $e) {
+            throw new \InvalidArgumentException("hydrate error in object '$this->name': " . $e->getMessage(), $e);
+        }
+    }
+
 }
