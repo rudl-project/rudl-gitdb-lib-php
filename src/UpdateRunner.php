e5c3c3454cfb24025ba1c30ecd3e83cd03395fb2
Matthes <matthes@leuffen.de>
2022-02-13 17:40:08 +0100

    added max sleep time on error

--- src/UpdateRunner.php
+++ src/UpdateRunner.php
@@ -8,6 +8,8 @@ class UpdateRunner
 {
     const ON_ERROR_SLEEP_TIME = 10;
 
+    const ON_ERROR_MAX_SLEEP_TIME = 60;
+    
     private $errorCount = 0;
 
     private $runCount = 0;
@@ -43,7 +45,12 @@ class UpdateRunner
         }
 
         sleep ($this->defaultSleepTime);
-        sleep (self::ON_ERROR_SLEEP_TIME * $this->errorCount);
+        
+        $errorSleep = self::ON_ERROR_SLEEP_TIME * $this->errorCount;
+        if ($errorSleep > self::ON_ERROR_MAX_SLEEP_TIME)
+            $errorSleep = self::ON_ERROR_MAX_SLEEP_TIME;
+        
+        sleep ($errorSleep);
     }
 
     public function run(callable $fn)
